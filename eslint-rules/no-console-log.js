5c6793c7f43e30612768ae752a229647ccbb3611
Milena Fluck <milena.fluck@adesso.de>
2025-04-05 14:48:21 +0200

    Beispiel-Regel erweitert

--- eslint-rules/no-console-log.js
+++ eslint-rules/no-console-log.js
@@ -1,12 +1,24 @@
 module.exports = {
+  // enthält metadata für eine Regel
   meta: {
-    type: 'problem',
+    type: 'problem', // alternativ "suggestion" oder "layout"
+    /***
+     - **Problem**: Die Regel identifiziert Code, der entweder einen Fehler verursacht oder zu verwirrendem Verhalten führen könnte.
+     EntwicklerInnen sollten dies als hohe Priorität ansehen, um es zu beheben.
+     - **Vorschlag**: Die Regel identifiziert Dinge, die besser gemacht werden könnten, aber keine Fehler verursachen, wenn der Code nicht
+     geändert wird.
+     - **Layout**: Die Regel kümmert sich um Whitespace, Semikolons, Kommas und Klammern – alles, was das Aussehen des Codes betrifft,
+     aber nicht seine Ausführung.
+     ***/
     docs: {
-      description: 'disallow the use of console.log',
-      category: 'Possible Errors',
-      recommended: true
+      description: 'disallow the use of console.log', // Eine kurze Beschreibung der Regel
+      category: 'Possible Errors', // Einteilung in logische Kategorien: z. B. "Best Practices", "Stylistic Issues", "Angular"
     },
-    schema: []
+    // Das `messages`-Objekt in den Metadaten einer ESLint-Regel definiert Fehlermeldungen mit dynamischen, platzhalterbasierten Nachrichten,
+    // die an spezifische Code-Verstöße angepasst werden können.
+    messages: {
+      noConsoleLog: "No console method if it is '{{ methodName }}'. Und was ich den Entwicklerinnen sonst noch so erzählen möchte..."
+    }
   },
 
   create(context) {
@@ -21,7 +33,10 @@ module.exports = {
         ) {
           context.report({
             node,
-            message: 'Unexpected console.log statement. Und was ich den Entwicklerinnen sonst noch so erzählen möchte...',
+            messageId: 'noConsoleLog',
+            data: {
+              methodName: node.callee.property.name,
+            }
           });
         }
       }
