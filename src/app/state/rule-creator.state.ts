09988731596ac2593171496fd2a63599c2d58646
Milena Fluck <milena.fluck@adesso.de>
2025-04-07 09:35:33 +0200

    Export functionality

--- src/app/state/rule-creator.state.ts
+++ src/app/state/rule-creator.state.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { State, StateToken } from '@ngxs/store';
+import { Action, State, StateContext, StateToken } from '@ngxs/store';
 import { createDefault, RuleCreatorStateModel } from './model';
+import { RuleCreatorActions } from './rule-creator.actions';
 
 export const RULE_CREATOR_STATE_TOKEN = new StateToken<RuleCreatorStateModel>('rule_creator');
 
@@ -10,4 +11,22 @@ export const RULE_CREATOR_STATE_TOKEN = new StateToken<RuleCreatorStateModel>('r
 })
 @Injectable()
 export class RuleCreatorState {
+
+  @Action(RuleCreatorActions.Export)
+  export(ctx: StateContext<RuleCreatorStateModel>): void {
+    const state = ctx.getState();
+    const rule = state.ruleForm.model?.rule;
+    const ruleTest = state.ruleForm.model?.ruleTest;
+    if (rule) this.exportToJsFile(rule, 'rule');
+    if (ruleTest) this.exportToJsFile(ruleTest, 'rule.spec');
+  }
+
+  private exportToJsFile(jsString: string, filename: string) {
+    const blob = new Blob([jsString], { type: 'application/javascript' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = `${filename}.js`;
+    link.click();
+    URL.revokeObjectURL(link.href);
+  }
 }
